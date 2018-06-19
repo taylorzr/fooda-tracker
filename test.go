149d233da0022008b48be5e1d7d9c7fc3b3d11f7
Zach Taylor <taylorzr@gmail.com>
2018-06-19 00:50:27 -0500

    Update working

--- test.go
+++ test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -11,11 +12,12 @@ import (
 
 var (
 	dynamo *dynamodb.DynamoDB
+	table  = "fooda"
 )
 
 func init() {
 	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-2"),
+		Region: aws.String("us-east-1"),
 	})
 
 	dynamo = dynamodb.New(sess)
@@ -34,26 +36,27 @@ type (
 )
 
 func main() {
-	user, err := getUser("zach.taylor@avant.com")
+	// user, err := getUser("zach.taylor@avant.com")
 
-	if err != nil {
-		panic(err)
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Printf("%v\n", user)
+	// fmt.Printf("%v\n", user)
 
-	newUser, err := createUser("notzach.taylor@avant.com")
+	// newUser, err := createUser("notzach.taylor@avant.com")
 
-	if err != nil {
-		panic(err)
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Println(newUser)
+	// fmt.Println(newUser)
+	updateUser()
 }
 
 func getUser(email string) (User, error) {
 	result, err := dynamo.GetItem(&dynamodb.GetItemInput{
-		TableName: aws.String("Fooda"),
+		TableName: aws.String(table),
 		Key: map[string]*dynamodb.AttributeValue{
 			"email": {
 				S: aws.String(email),
@@ -76,6 +79,40 @@ func getUser(email string) (User, error) {
 	return user, nil
 }
 
+func updateUser() {
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":order": {
+				L: []*dynamodb.AttributeValue{
+					{
+						M: map[string]*dynamodb.AttributeValue{
+							"ordered_at": {
+								S: aws.String(time.Now().Format(time.RFC3339))},
+						},
+					},
+				},
+			},
+		},
+		TableName: aws.String(table),
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String("zach.taylor@avant.com"),
+			},
+		},
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String("set orders = list_append(orders, :order)"),
+	}
+
+	_, err := dynamo.UpdateItem(input)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Successfully updated 'zach.taylor@avant.com'")
+}
+
 func createUser(email string) (User, error) {
 	user := User{
 		Email: email,
@@ -91,7 +128,7 @@ func createUser(email string) (User, error) {
 	}
 
 	input := &dynamodb.PutItemInput{
-		TableName: aws.String("Fooda"),
+		TableName: aws.String(table),
 		Item:      item,
 	}
 
