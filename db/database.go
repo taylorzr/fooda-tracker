c5dd9cc556a8e2348672d79f8904ce67dea480cf
Zach Taylor <taylorzr@gmail.com>
2018-06-20 08:34:15 -0500

    WIP

--- db/database.go
+++ db/database.go
@@ -1,86 +1,198 @@
 package db
 
-import(
+import (
+	"fmt"
 	"time"
 
-  "github.com/jinzhu/gorm"
-
-	"github.com/taylorzr/fooda-tracker/fooda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-var db *gorm.DB
+var (
+	db    *dynamodb.DynamoDB
+	table = "fooda"
+)
 
 func init() {
-	var err error
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
 
-	db, err = gorm.Open("sqlite3", "fooda-tracker.db")
+	db = dynamodb.New(sess)
+}
+
+type (
+	User struct {
+		Email  string  `json:"email"`
+		Orders []Order `json:"orders"`
+	}
+
+	Order struct {
+		Time  string `json:"time"`
+		Items string `json:"items"`
+	}
+)
+
+type Orders struct {
+	Date     string   `json:"date"`
+	Orderers []string `json:"orderers"`
+}
+
+func main() {
+	// user, err := getUser("zach.taylor@avant.com")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("%v\n", user)
+
+	// newUser, err := createUser("notzach.taylor@avant.com")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(newUser)
+	// updateUser()
+	getTodaysOrders()
+	users, _ := getUsers()
+	fmt.Println(users)
+}
+
+func getUser(email string) (User, error) {
+	result, err := db.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(table),
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+	})
 
 	if err != nil {
-		panic("failed to connect database")
+		return User{}, err
 	}
 
-	// defer db.Close()
+	user := User{}
 
-	// Migrate the schema
-	db.AutoMigrate(
-		&fooda.Order{},
-		&fooda.User{},
-	)
-}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &user)
 
-func GetOrder(email string) fooda.Order {
-	var order fooda.Order
-
-	db.First(&order, "email = ?", email)
-
-	return order
-}
-
-func CreateOrder(email string, orderedAt time.Time) fooda.Order {
-	user := findOrCreateUser(email)
-
-	// TODO: Correct syntax?!?
-	order := fooda.Order{ OrderedAt: orderedAt, User: user }
-	db.Create(&order)
-
-	return order
-}
-
-func LastRun() time.Time {
-	var run fooda.Run
-
-	// TODO: Is this a valid method?
-	db.Last(&run)
-
-	return run.CreatedAt
-}
-
-func TodaysOrders() []fooda.Order {
-	var orders []fooda.Order
-
-	// TODO: Find orders for today only!!!
-	db.Find(&orders)
-
-	return orders
-}
-
-func AllUsers() []fooda.User {
-	var users []fooda.User
-
-	db.Find(&users)
-
-	return users
-}
-
-func findOrCreateUser(email string) fooda.User {
-	var user fooda.User
-
-	db.First(&user, "email = ?", email)
-
-	if user.ID == 0 {
-		user = fooda.User{ Email: email }
-		db.Create(&user)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
 	}
 
-	return user
+	return user, nil
 }
+
+func updateUser() {
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":order": {
+				L: []*dynamodb.AttributeValue{
+					{
+						M: map[string]*dynamodb.AttributeValue{
+							"ordered_at": {
+								S: aws.String(time.Now().Format(time.RFC3339))},
+						},
+					},
+				},
+			},
+		},
+		TableName: aws.String(table),
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String("zach.taylor@avant.com"),
+			},
+		},
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String("set orders = list_append(orders, :order)"),
+	}
+
+	_, err := db.UpdateItem(input)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Successfully updated 'zach.taylor@avant.com'")
+}
+
+func getUsers() ([]string, error) {
+	r, err := db.Scan(&dynamodb.ScanInput{
+		TableName: aws.String("fooda_users"),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	users := []string{}
+
+	for _, item := range r.Items {
+		users = append(users, *item["email"].S)
+	}
+
+	return users, nil
+}
+
+func getTodaysOrders() {
+	result, err := db.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("fooda_orders"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"date": {
+				S: aws.String("2014-11-15"),
+			},
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	orders := Orders{}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &orders)
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	fmt.Printf("%#v\n", orders)
+}
+
+func createUser(email string) (User, error) {
+	user := User{
+		Email: email,
+		Orders: []Order{
+			Order{Time: "2014-11-16", Items: "Some more items"},
+		},
+	}
+
+	item, err := dynamodbattribute.MarshalMap(user)
+
+	if err != nil {
+		return user, err
+	}
+
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String(table),
+		Item:      item,
+	}
+
+	_, err = db.PutItem(input)
+
+	return user, err
+}
+
+/*
+When I receive an order email
+store date & email
+
+At 9:45am each morning, notify each user if they haven't ordered
+  - need to be able to retrieve all users
+	- need to be able to retrieve all orders for today
+*/
